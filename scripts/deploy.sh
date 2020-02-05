b0940a8afeb7228d74d044779b91a8fa204073f2
Alexander Pushkov <alexander@notpushk.in>
2020-02-05 18:27:23 +0300

    Deploy every commit to master on deploy.sh (#6)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,8 +2,7 @@
 
 shopt -s nullglob
 cd app/dist/
-ls
-for f in *.{dmg,zip,deb,rpm,msi}; do
-  curl -F "fileToUpload=@$f" -F 'reqtype=fileupload' 'https://catbox.moe/user/api.php'
+for f in *.{dmg,zip,deb,rpm}; do
+  curl --upload-file "$f" "https://transfer.sh/$f"
   printf "\n"
 done
