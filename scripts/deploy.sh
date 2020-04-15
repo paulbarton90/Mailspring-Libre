1d84431f3d45309f212daed49e588cd9e503dc0c
Nate Ijams <exprezdev@pm.me>
2020-04-15 01:06:24 -0500

    Add windows build to Travis CI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,8 @@
 
 shopt -s nullglob
 cd app/dist/
-for f in *.{dmg,zip,deb,rpm}; do
+ls
+for f in *.{dmg,zip,deb,rpm,msi}; do
   curl -F "fileToUpload=@$f" -F 'reqtype=fileupload' 'https://catbox.moe/user/api.php'
   printf "\n"
 done
